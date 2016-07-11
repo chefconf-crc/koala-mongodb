8e5180e5763cc52ad0ec41387a4a4e37af9476d1
Stephen Griffin <stepheng@achievers.com>
2016-07-11 17:33:48 +0000

    changes for cookbook

---
+++ recipes/install_mongo.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: .
+# Recipe:: install_mongo
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+
+yum_repository 'mongodb-org-3.2' do
+  description 'MongoDB Repository'
+  baseurl 'https://repo.mongodb.org/yum/redhat/$releaserver/mongodb-org/3.2/x86_64/'
+  gpgcheck true
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+  enabled true
+  action :create
