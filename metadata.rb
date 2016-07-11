c95f9dc34a22b2f43af9c538d70934571714929b
Allison Cappelli <alliecappelli@gmail.com>
2016-07-11 17:18:50 +0000

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'koala-mongodb'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures koala-mongodb'
+long_description 'Installs/Configures koala-mongodb'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: koala-mongodb
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
